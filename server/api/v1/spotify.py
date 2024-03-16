1e16f52dfd79a95d324d6f1eae42bdb5df4240c1
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-03-16 10:28:59 +0100

    BE: implemented communicstion with spoitfy to get albums, playlists and tracks

--- server/api/v1/spotify.py
+++ server/api/v1/spotify.py
@@ -12,26 +12,83 @@ class SpotifyAPI:
     def __init__(self, client_id, client_secret):
         """Initialize spotify client
         """
-        client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
-                                                              client_secret=client_secret)
-        self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
+        try:
+            client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
+                                                                  client_secret=client_secret)
+            self.sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+            self.initialization_successful = True
+        except Exception as e:
+            print("Error fetching featured playlist:", e)
+            self.initialization_successful = False
+        
     def fetch_featured_playlist(self):
         """
-        Method to fetch playlist
+        Method to fetch featured playlist
         """
-        playlist = self.sp.featured_playlists()
-        return playlist
-    
-    def fetch_single_track(self):
+        # Check if spoify client is initialized succesfully
+        if not self.initialization_successful:
+            return {"error": "Spotify client initialization failed"}
+        
+        try:
+            playlist = self.sp.featured_playlists()
+            return playlist
+        except Exception as e:
+            print("Error fetching featured playlist:", e)
+            return {"error": "Error fetching playlist"}
+        
+    def fetch_new_releases(self):
+        """ Method to fetch new release
         """
-        Method to fetch playlist
+         # Check if spoify client is initialized succesfully
+        if not self.initialization_successful:
+            return {"error": "Spotify client initialization failed"}
+        
+        try:
+            release = self.sp.new_releases()
+            return release
+        except Exception as e:
+            print("Error fetching featured new release:", e)
+            return {"error": "Error fetching new release"}
+        
+    def get_playlist_tracks(self, playlist_uri):
+        """ Comment
         """
-        track = self.sp.featured_playlists()
-        return track
-    
-    def fetch_playlist_tracks(self, playlist_uri):
+        # Check if spoify client is initialized succesfully
+        if not self.initialization_successful:
+            return {"error": "Spotify client initialization failed"}
+        try:
+            playlist_id = playlist_uri.split(':')[-1]
+            tracks = self.sp.playlist_tracks(playlist_id)
+            return tracks['items']
+        except Exception as e:
+            print("Error fetching tracks", e)
+            return {"error": "Error fetching tracks"}
+        
+    def get_album_tracks(self, album_id):
+        """ Gets album tracks
         """
-        Fetch playlist uri
+        # Check if spoify client is initialized succesfully
+        if not self.initialization_successful:
+            return {"error": "Spotify client initialization failed"}
+        try:
+            tracks = self.sp.album_tracks(album_id)
+            return tracks
+        except Exception as e:
+            print("Error fetching tracks", e)
+            return {"error": "Error fetching tracks"}
+        
+    def get_track(self, track_id):
+        """ Get  track
         """
-        tracks = self.sp.playlist_tracks(playlist_uri)
+          # Check if spoify client is initialized succesfully
+        if not self.initialization_successful:
+            return {"error": "Spotify client initialization failed"}
+        
+        try:
+            track = self.sp.track(track_id)
+            return track
+        except Exception as e:
+            print("Error fetching track", e)
+            return {"error": "Error fetching track"}
+        
+        
--- server/api/v1/view/spotifyAPI.py
+++ server/api/v1/view/spotifyAPI.py
@@ -10,17 +10,38 @@ client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
 spotify_api = SpotifyAPI(client_id=client_id, client_secret=client_secret)
 
 
-@spotify.route('/featured_playlist/')
+@spotify.route('/featured_playlist/', methods=['GET'], endpoint='featured_playlist')
 def featured_playlist():
     """ Gets featured playlist
     """
     playlist = spotify_api.fetch_featured_playlist()
     return jsonify(playlist)
 
-@spotify.route('/featured_playlist/<playlist_uri>/')
-def playlist_tracks(playlist_uri):
-    """ Gets Playlist tracks
+@spotify.route('/new_release/', methods=['GET'], endpoint='new_release')
+def new_release():
+    """ Gets new release
     """
-    if playlist_uri:
-        tracks = spotify_api.fetch_playlist_tracks(playlist_uri)
-        return jsonify(tracks)
+    release = spotify_api.fetch_new_releases()
+    return jsonify(release)
+
+
+@spotify.route('/playlist/<playlist_id>/tracks', methods=['GET'])
+def playlist_tracks(playlist_id):
+    """ Gets new release
+    """
+    tracks = spotify_api.get_playlist_tracks(playlist_id)
+    return jsonify(tracks)
+
+@spotify.route('/album/<album_id>/tracks', methods=['GET'])
+def album_tracks(album_id):
+    """ Gets new release
+    """
+    tracks = spotify_api.get_album_tracks(album_id)
+    return jsonify(tracks)
+
+@spotify.route('/track/<track_id>', methods=['GET'])
+def track(track_id):
+    """ Gets new release
+    """
+    track = spotify_api.get_track(track_id)
+    return jsonify(track)
