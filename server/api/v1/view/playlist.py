53bc1d06a4032828b2c7dea6959e34f5cf637c72
Kingsley Akpan <54294050+Khingz@users.noreply.github.com>
2024-03-17 19:24:16 +0100

    BE: updates to playlist routes

--- server/api/v1/view/playlist.py
+++ server/api/v1/view/playlist.py
@@ -8,15 +8,32 @@ from flask_login import current_user, login_required
 playlist = Blueprint('playlist', __name__)
 
 @playlist.route('/playlists', methods=['GET'], endpoint='playlists')
+@login_required
 def all_playlists():
     """ Get all playlist
     """
+    if not current_user.is_authenticated:
+        return jsonify({'messasge': 'please login to conitnue'}), 301
     db_playlist = storage.all(Playlist).values()
-    print(db_playlist)
     playlist = []
-    # print(playlist)
     for item in db_playlist:
         playlist.append(item.to_dict())
+    return jsonify({'playlist': playlist}), 200
+
+@playlist.route('/playlists/<user_id>', methods=['GET'])
+@login_required
+def all_user_playlist(user_id):
+    """ Get a user playlist
+    """
+    if not current_user.is_authenticated:
+        return jsonify({'messasge': 'please login to conitnue'}), 301
+    if not user_id:
+        return jsonify({'message': 'playlist no found'}), 404
+    db_playlist = storage.all(Playlist).values()
+    playlist = []
+    for item in db_playlist:
+        if item.user_id == user_id:
+            playlist.append(item.to_dict())
     return jsonify({'playlist': playlist}), 200
 
 @playlist.route('playlist/new_playlist', methods=['POST'])
@@ -46,9 +63,29 @@ def single_playlist(playlist_id):
     """
     playlist = storage.get(Playlist, playlist_id)
     if playlist:
+        print(playlist)
         return jsonify(playlist.to_dict()), 200
     else:
         return jsonify({'message': 'playlist not found'}), 404
+    
+@playlist.route('playlist/update_playlist/<playlist_id>', methods=['POST'])
+@login_required
+def update_playlist(playlist_id):
+    """ Add a nw playlist
+    """
+    if not current_user.is_authenticated:
+        return jsonify({'messasge': 'please login to conitnue'}), 301
+    playlist = storage.get(Playlist, playlist_id)
+    if playlist.user_id != current_user.id:
+        return jsonify({'message': 'you cannot not update this playlist'}), 301
+    playlist.name = request.form.get('name')
+    playlist.description = request.form.get('description')
+    playlist.primary_color = request.form.get('primary_color')
+    if not playlist.name  or playlist.name == '':
+        return jsonify({'error': 'all fields are required'}), 400
+    playlist.save()
+    return jsonify({'message': 'playlist updated successfully'})
+
     
 @playlist.route('playlist/<playlist_id>', methods=['DELETE'])
 def delete_playlist(playlist_id):
